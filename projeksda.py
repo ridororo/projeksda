a5d6aa7db8ace954ccd963a0ea7d98a4bc14e132
talithasalsabila19 <jiminsayang1995@gmail.com>
2025-05-30 21:23:58 +0700

    menampilkan gambar & mengubah warna bg

--- projeksda.py
+++ projeksda.py
@@ -15,3 +15,21 @@ class AplikasiTkinter:
         image = Image.open("background.png")
         self.bg_image = ImageTk.PhotoImage(image)
         self.bg_label = tk.Label(self.root, image=self.bg_image)
+     
+     self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        self.root.resizable(True, True)
+
+        self.tampilkan_gambar()
+ 
+
+    def tampilkan_gambar(self):
+        img = Image.open("sehun.png") 
+        img = img.resize((400,250))
+        self.root.configure(bg="#D9F2F4")  
+
+        self.photo = ImageTk.PhotoImage(img)
+
+        self.label_gambar = tk.Label(self.root, image=self.photo, bg="#D9F2F4")
+
+        self.label_gambar.pack(pady=10)
