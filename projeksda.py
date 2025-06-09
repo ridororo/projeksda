3ded689f0e6dae355c13b7db4d7409758fa15a2a
talithasalsabila19 <jiminsayang1995@gmail.com>
2025-06-09 20:00:17 +0700

    menambahkan bg dan mengecek bg ada atau tidak

--- projeksda.py
+++ projeksda.py
@@ -18,25 +18,29 @@ class AplikasiTkinter:
         self.default_bg_path = "background.png"
         self.new_page_bg_path = "bg2.png"        
 
-    def tampilkan_gambar(self):
-        img = Image.open("sehun.png") 
-        img = img.resize((400,250))
-        self.root.configure(bg="#D9F2F4")  
+   try:
+            
+            original_bg_image_pil = Image.open(self.default_bg_path)
+            resized_bg_image_pil = original_bg_image_pil.resize((self.screen_width, self.screen_height), Image.LANCZOS)
+            self.photo_background_default = ImageTk.PhotoImage(resized_bg_image_pil)
+            
+           
+            original_new_page_bg_pil = Image.open(self.new_page_bg_path)
+            resized_new_page_bg_pil = original_new_page_bg_pil.resize((self.screen_width, self.screen_height), Image.LANCZOS)
+            self.photo_background_new_page = ImageTk.PhotoImage(resized_new_page_bg_pil)
 
-        self.photo = ImageTk.PhotoImage(img)
-
-        self.label_gambar = tk.Label(self.root, image=self.photo, bg="#D9F2F4")
-
-        self.label_gambar.pack(pady=10)
-     
-        self.button =tk.Button(
-    self.root,
-    text="Login",
-    command=self.tampilkan_pesan,
-    font=("Poppins", 14),  
-    width=20,                
-    height=2                 
-).pack(pady=10)
+            self.bg_label = tk.Label(self.root, image=self.photo_background_default) 
+            self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+            self.content_bg_color = "#D9F2F4" 
+    
+        except FileNotFoundError as e:
+            print(f"Error: File background tidak ditemukan ({e}). Menggunakan warna latar belakang default.")
+            self.content_bg_color = "#D9F2F4" 
+            self.root.configure(bg=self.content_bg_color)
+            self.bg_label = tk.Label(self.root, bg=self.content_bg_color)
+            self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+            self.photo_background_default = None 
+            self.photo_background_new_page = None
 
         self.button =tk.Button(
     self.root,
